2c5be2e1fa58cfa1988d7cee52f85fcabcf73aa3
jisukJ <78594351+jisukJ@users.noreply.github.com>
2021-03-26 00:08:49 +0900

    V! E! C! T! O! R!

--- Project1/vector_geometry.cpp
+++ Project1/vector_geometry.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cmath>
-
+#include <vector>
 using namespace std;
 
+const double EPSILON = 1e-9;
+const double INFTY = 1e200;
 const double PI = 2.0*acos(0.0);
 
 struct vector2 {
@@ -70,7 +72,7 @@ double ccw(vector2 p, vector2 a, vector2 b) {
 
 bool lineIntersection(vector2 a, vector2 b, vector2 c, vector2 d, vector2& x) {
 	double det = (b - a).cross(d - c);
-	if (fabs(det) < LDBL_EPSILON) {
+	if (fabs(det) < EPSILON) {
 		return false;
 	}
 	x = a + (b - a)*((c - a).cross(d - c) / det);
@@ -131,6 +133,81 @@ vector2 perpendicularFoot(vector2 p, vector2 a, vector2 b) {
 double pointToLine(vector2 p, vector2 a, vector2 b) {
 	return (p - perpendicularFoot(p, a, b)).norm();
 }
+
+//PINBALL
+int C, N;
+vector2 center[50];
+int radius[50];
+
+vector<double> solve2(double a, double b, double c) {
+	double d = b * b - 4 * a * c;
+	if (d < -EPSILON) {
+		return vector<double>();
+	}
+	if (d < EPSILON) {
+		return vector<double>(1, -b / (2 * a));
+	}
+	vector<double>ret;
+	ret.push_back((-b - sqrt(d)) / (2 * a));
+	ret.push_back((-b + sqrt(d)) / (2 * a));
+	return ret;
+}
+
+double hitCircle(vector2 here, vector2 dir, vector2 center, int radius) {
+	double a = dir.dot(dir);
+	double b = 2 * dir.dot(here - center);
+	double c = center.dot(center) + here.dot(here) - 2 * here.dot(center) - radius * radius;
+	vector<double> sols = solve2(a, b, c);
+	if (sols.empty() || sols[0] < EPSILON) {
+		return INFTY;
+	}
+	return sols[0];
+}
+
+vector2 reflect(vector2 dir, vector2 center, vector2 contact) {
+	return (dir - dir.project(contact - center) * 2).normalize();
+}
+
+void simulate(vector2 here, vector2 dir) {
+	dir = dir.normalize();
+	int hitCount = 0;
+	while (hitCount < 100) {
+		int cir = -1;
+		double time = INFTY * 0.5;
+		for (int i = 0; i < N; ++i) {
+			double cand = hitCircle(here, dir, center[i], radius[i] + 1);
+			if (cand < time) {
+				time = cand;
+				cir = i;
+			}
+		}
+		if (cir == -1) {
+			break;
+		}
+		if (hitCount++) {
+			cout << " ";
+		}
+		cout << cir;
+		vector2 contact = here + dir * time;
+		dir = reflect(dir, center[cir], contact);
+		here = contact;
+	}
+	cout << endl;
+}
+
 int main()
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> C;
+	for (int test = 1; test <= C; ++test) {
+		double x, y, dx, dy;
+		cin >> x >> y >> dx >> dy >> N;
+		vector2 here(x, y);
+		vector2 dir(dx, dy);
+		for (int i = 0; i < N; ++i) {
+			cin >> center[i].x >> center[i].y >> radius[i];
+		}
+		simulate(here, dir);
+	}
 }
